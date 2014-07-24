1db291d5e3446e28bbff7313ac7d60a0cc347a55
andrew.chaa <andrew.chaa@huddle.com>
2014-07-24 18:01:36 +0100

    adding spinner for time delay

--- app/src/main/java/uk/me/andrewchaa/yowifi/MainActivity.java
+++ app/src/main/java/uk/me/andrewchaa/yowifi/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Spinner;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +13,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner timeSpinner 
     }
 
 
